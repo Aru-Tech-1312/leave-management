fe03c8e294f216558f7ff9ad6e6adfbb0a944116
Aru-Tech-1312 <ut01312tic2024@gmail.com>
2024-11-10 01:29:03 +0530

    edited

--- frontend/views/leaves/leaveRequest.js
+++ frontend/views/leaves/leaveRequest.js
@@ -15,7 +15,8 @@ if(loggedInUser.role == 'Admin'){
 
     // Display welcome message
     const welcomeMessage = document.getElementById('welcomeMessage');
-    welcomeMessage.textContent = `Welcome, ${loggedInUser.firstName} ${loggedInUser.lastName}`;
+    welcomeMessage.textContent = `Welcome, 
+    ${loggedInUser.firstName} ${loggedInUser.lastName}`;
 
     // Logout button functionality
     const logoutButton = document.getElementById('logoutButton');
